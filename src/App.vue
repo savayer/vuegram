9f092e3d5b5d5463dfb0ada92eb06eae6a3c2562
savayer <sava0094@gmail.com>
2018-09-08 12:42:45 +0700

    first steps

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="app">
-    
+    <div class="app-phone">
+      <div class="phone-header">
+        <img src="./images/vue_gram_logo_cp.png" />
+      </div>
+      <phone-body />
+      <div class="phone-footer">
+        <div class="home-cta">
+        <i class="fas fa-home fa-lg"></i>
+       </div>
+       <div class="upload-cta">
+        <i class="far fa-plus-square fa-lg"></i>
+       </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import PhoneBody from "./components/PhoneBody.vue";
+
   export default {
-    name: 'app',
-  }
+    name: "app",
+    components: {
+      PhoneBody
+    }
+  };
 </script>
 
 <style lang="scss" src="./styles/app.scss"></style>
---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="phone-body">
+        Phone body
+    </div>
+</template>
+<script>
+  export default {
+    name: 'PhoneBody'
+  }
+</script>
+
+<style lang="scss" src="../styles/phone-body.scss"></style>
+
