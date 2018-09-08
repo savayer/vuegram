bbd1df8db94fbd9b475e1bc6e22ab09031e46459
savayer <sava0094@gmail.com>
2018-09-08 16:06:50 +0700

    I get it i should use component here, because i use v-for, and would be better to create seperate file with description

--- src/components/PhoneBody.vue
+++ src/components/PhoneBody.vue
@@ -1,7 +1,26 @@
 <template>
     <div class="phone-body">
         <div class="feed">
-            <vuegram-post v-for="(post, index) in posts" :post="post" :key="index"/>
+            <vuegram-post 
+              v-for="(post, index) in posts" 
+              :key="index"
+              :post="post"
+              v-if="step === 1" />
+            
+              <div class="selected-image"
+                   :style="{ backgroundImage: 'url(' + image + ')' }"></div>
+              <div class="filter-container">
+                <div class="filter-type"
+                     v-for="(filter, indexFilter) in filters"
+                     :key="indexFilter">
+                  <p>{{ filter.name }}</p>
+                  <div class="img" :class="filter.name"
+                       :style="{ backgroundImage: 'url(' + image + ')' }">
+                  </div>
+                </div>
+              </div>
+            
+            
         </div>
     </div>
 </template>
@@ -12,7 +31,10 @@
     name: 'PhoneBody',
     props: {
       posts: Array,
-      filters: Array
+      filters: Array,
+      step: Number,
+      image: String,
+      selectedFilter: String
     },
     components: {
       VuegramPost
